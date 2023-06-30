6b7425e01c3b3089d60b4ca7bd0a542131a96d44
P. Shiva krishna <117880640+SHIVA-6699@users.noreply.github.com>
2023-06-30 17:55:44 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,5 @@
-public class demo
-{
-public static void main(String a[])
-{
-System.out.println("Hello");
-}
+class demo {
+  public static void main(String a[]) {
+    System.out.println("Hello");
+  }
 }
